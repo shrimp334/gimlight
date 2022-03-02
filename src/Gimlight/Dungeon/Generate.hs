d80a3db5092f12b8a51f9a4d7af3d34bda1dd227
Hiroki Tokunaga <tokusan441@gmail.com>
2022-03-02 18:19:51 +0900

    refactor: use `foldlM` (#465)
    * refactor: use `foldl`
    
    * refactor: use `foldlM`

--- src/Gimlight/Dungeon/Generate.hs
+++ src/Gimlight/Dungeon/Generate.hs
@@ -242,25 +242,24 @@ placeEnemies ::
     -> Room
     -> Int
     -> StateT IndexGenerator (State StdGen) CellMap
-placeEnemies _ cm _ 0 = return cm
-placeEnemies tc cm r n = do
-    x <- lift $ randomRST (x1 r, x2 r - 1)
-    y <- lift $ randomRST (y1 r, y2 r - 1)
-    enemy <- newMonster
-    let newMap =
-            fromRight cm $ flip execStateT cm $ locateActorAt tc enemy (V2 x y)
-    placeEnemies tc newMap r (n - 1)
+placeEnemies tc before r n = foldlM foldStep before [1 .. n]
+  where
+    foldStep cm _ = do
+        x <- lift $ randomRST (x1 r, x2 r - 1)
+        y <- lift $ randomRST (y1 r, y2 r - 1)
+        enemy <- newMonster
+        return $ fromRight cm $ flip execStateT cm $
+            locateActorAt tc enemy (V2 x y)
 
 placeItems :: CellMap -> TileCollection -> Room -> Int -> State StdGen CellMap
-placeItems cm _ _ 0 = return cm
-placeItems cm tc r n = do
-    x <- randomRST (x1 r, x2 r - 1)
-    y <- randomRST (y1 r, y2 r - 1)
-    newItem <- choiceST items
-    let newMap =
-            fromRight cm $ flip execStateT cm $ locateItemAt tc newItem (V2 x y)
-    placeItems newMap tc r (n - 1)
+placeItems before tc r n = foldlM foldStep before [1 .. n]
   where
+    foldStep cm _ = do
+        x <- randomRST (x1 r, x2 r - 1)
+        y <- randomRST (y1 r, y2 r - 1)
+        newItem <- choiceST items
+        return $ fromRight cm $ flip execStateT cm $
+            locateItemAt tc newItem (V2 x y)
     items =
         [ liftUnion herb
         , liftUnion sampleBook
